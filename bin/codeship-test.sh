0d1a5a88082e4b39c926d7cc4a8cdf277c7a76dc
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-24 09:42:13 +1000

    Fixed codeship test to run Polymer1.0 tests on both the Polymer branch and all version releases

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -5,7 +5,7 @@ echo "Testing branch: ${CI_BRANCH}"
 # REDO WHEN POLYMER 1.0 GOES LIVE
 
 # If polymer 1.0
-if [ ${CI_BRANCH} == "polymer1.0" ]; then
+if [ ${CI_BRANCH} == "polymer1.0" ] || [ {$CI_BRANCH} == v* ]; then
 
     if [ ${PIPE_NUM} == 1 ]; then
         # Run local tests
@@ -17,7 +17,6 @@ if [ ${CI_BRANCH} == "polymer1.0" ]; then
         echo "Pipe not used"
     fi
 else
-    # make sure wct-sauce plugin works with old version of wct
     cd ../uqlibrary-elements
     ./bin/elements_local_tests.sh
     cd ../uqlibrary-computers
