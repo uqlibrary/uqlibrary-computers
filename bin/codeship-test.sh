f88e4626683b8a5e2b1236e701d018f50a5c2573
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-24 13:47:49 +1000

    Brand new test script for Polymer1.0 - removing old master code

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -2,23 +2,16 @@
 
 echo "Testing branch: ${CI_BRANCH}"
 
-# REDO WHEN POLYMER 1.0 GOES LIVE
+if [ ${CI_BRANCH} != "GH_PAGES" ] && [ {$PIPE_NUM} == 1 ]; then
+    # Run local tests
+    echo "Installing global"
+    npm install -g bower web-component-tester
 
-# If polymer 1.0
-if [ ${CI_BRANCH} == "polymer1.0" ] || [ {$CI_BRANCH} == v* ]; then
+    echo "Installing bower dependencies"
+    bower install
 
-    if [ ${PIPE_NUM} == 1 ]; then
-        # Run local tests
-        echo "Starting local WCT tests"
-        npm install web-component-tester -g
-        bower install
-        wct
-    else
-        echo "Pipe not used"
-    fi
+    echo "Starting local WCT tests"
+    wct
 else
-    cd ../uqlibrary-elements
-    ./bin/elements_local_tests.sh
-    cd ../uqlibrary-computers
-    ../uqlibrary-elements/bin/sauce.sh
+    echo "Pipe not used."
 fi
