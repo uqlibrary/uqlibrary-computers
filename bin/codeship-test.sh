f74d11fafbe5a52df838d54084ca3f3cbc92c5c5
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-24 13:50:47 +1000

    Fixed issue where Polymer1.0 would not run any tests

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Testing branch: ${CI_BRANCH}"
+echo "Testing branch: ${CI_BRANCH} with pipe ${PIPE_NUM}"
 
-if [ ${CI_BRANCH} != "GH_PAGES" ] && [ {$PIPE_NUM} == 1 ]; then
+if [ ${CI_BRANCH} != "GH_PAGES" ] && [ ${PIPE_NUM} == 1 ]; then
     # Run local tests
     echo "Installing global"
     npm install -g bower web-component-tester
