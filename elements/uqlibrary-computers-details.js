777d9b7ccde0d1a57401c1a5df80ef19a45b9ac4
Cliff Lien <uqclien@gmail.com>
2016-06-07 17:41:57 +1000

    update api pkg

--- elements/uqlibrary-computers-details.js
+++ elements/uqlibrary-computers-details.js
@@ -60,13 +60,26 @@
       this.fire('close');
     },
     /**
+     * Generate the floor plan api url
+     * @private
+     */
+    _getFloorPlanUrl: function () {
+      if(this.building.buildingCode && this._selectedRoom.roomCode) {
+        return 'https://www.library.uq.edu.au/uqlsm/map.php?building=' + this.building.buildingCode + '&room=' + this._selectedRoom.roomCode;
+      }else {
+        return '';
+      }
+    },
+    /**
      * Show the floor plan in a new window, TODO: show the floor plan in a neon-animated-page
      * @private
      */
     _showFloorPlan: function (e) {
-      var room = e.model.item;
+      var room = e.model.item.item;
+      room.name = e.model.item.name;
+      this._selectedRoom = room;
 
-      window.open('https://www.library.uq.edu.au/uqlsm/map.php?building=' + this.building.buildingCode + '&room=' + room.item.roomCode, '_blank');
+      window.open(this._getFloorPlanUrl(), '_blank');
 
       this.$.ga.addEvent('Navigation', 'Floor plan view of ' + this.building.library + ' ' + room.name);
     }
